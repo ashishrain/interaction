3a37e54329a4e5e01738571fc5e3fd0c820aa7b8
Ashish Rai <raai.ashish@gmail.com>
2020-10-21 11:37:58 +0100

    creating week 2 component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,43 @@
 import React from 'react';
 import Week1 from './components/Week1';
+import Week2 from './components/Week2';
+
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+  } from "react-router-dom";
+
+
+const Homepage = () => (
+    <nav>
+        <h1>Interaction Design</h1>
+        <ul>
+            <li>
+              <Link to="/week-1"><h2>Week 1</h2></Link>
+            </li>
+            <li>
+              <Link to="/week-2"><h2>Week 2</h2></Link>
+            </li>
+        </ul>
+    </nav>
+);
 
 const App = () => (
-    <Week1 />
+    <Router>
+        <Switch>
+            <Route path="/week-1">
+                <Week1 />
+            </Route>
+            <Route path="/week-2">
+                <Week2 />
+            </Route>
+            <Route path="">
+                <Homepage />
+            </Route>
+        </Switch>
+    </Router>
 );
 
 
---
+++ src/components/Week2/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Week2 = () => {
+    return <h1>This is week 2</h1>
+};
+export default Week2;
