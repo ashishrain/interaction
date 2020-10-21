56451c62465a4fef5551d28f76486cd7f0c52420
Ashish Rai <raai.ashish@gmail.com>
2020-10-21 10:41:41 +0100

    Updates

---
+++ precache-manifest.dc46c07fe2d0aa87093a8d2996c54759.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9ff1e406518913f6a28f320c349c910",
+    "url": "/interaction/index.html"
+  },
+  {
+    "revision": "1a274d3bca51f8d9e376",
+    "url": "/interaction/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "ee5b07314529aaa3586e",
+    "url": "/interaction/static/js/2.f2b206ed.chunk.js"
+  },
+  {
+    "revision": "8d190e2d02f8e768921a63f56a741895",
+    "url": "/interaction/static/js/2.f2b206ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a274d3bca51f8d9e376",
+    "url": "/interaction/static/js/main.5806fa4e.chunk.js"
+  },
+  {
+    "revision": "5f81642ef4da6ccd4c1c",
+    "url": "/interaction/static/js/runtime-main.384a5f7c.js"
+  }
+]);
