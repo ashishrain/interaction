de1eed14c1b2b8b42c22a2f4df4de116968ef811
Ashish Rai <raai.ashish@gmail.com>
2020-11-27 20:19:31 +0000

    Created a week 3 component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Week1 from './components/Week1';
 import Week2 from './components/Week2';
+import Week3 from './components/Week2';
 
 import {
     BrowserRouter as Router,
@@ -20,6 +21,9 @@ const Homepage = () => (
             <li>
               <Link to="/week-2"><h2>Week 2</h2></Link>
             </li>
+            <li>
+                <Link to="week-3"><h2>Week 3</h2></Link>
+            </li>
         </ul>
     </nav>
 );
@@ -32,6 +36,9 @@ const App = () => (
             </Route>
             <Route path="/week-2">
                 <Week2 />
+            </Route>
+            <Route path="/week-3">
+                <Week3 />
             </Route>
             <Route path="">
                 <Homepage />
---
+++ src/components/Week 3/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Week3 = () => {
+    return <p>This is week 3 component.</p>
+};
+
+export default Week3;
